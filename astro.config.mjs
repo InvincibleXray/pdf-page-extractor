721d9d10193146274242f1d3ee9463d3cf369ac6
InvincibleXray <invinciblexray@gmail.com>
2026-08-12 11:50:34 +0530

    ci: add GitHub Actions workflow and config for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://invinciblexray.github.io',
+  base: '/pdf-page-extractor',
   integrations: [tailwind()]
 });
